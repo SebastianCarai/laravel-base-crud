598364e5c037b4a2ef573fea0996749db2ac9210
SebastianCarai <sebastiancarai7@gmail.com>
2022-02-17 14:49:24 +0100

    delete

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,7 +6,13 @@
         <div class="card-body">
             <h5 class="card-title">{{$comic->title}}</h5>
             <p class="card-text">{{$comic->description}}</p>
-            <a class="btn btn-primary" href="{{ route('comics.edit' , ['comic' => $comic->id]) }}">Edit</a>
+            <a class="btn btn-primary" href="{{ route('comics.edit' , ['comic' => $comic->id]) }}">Edit Card</a>
+
+            <form action="{{ route('comics.destroy' , ['comic' => $comic->id]) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger my-2">Delete Card</button>
+            </form>
         </div>
 
     </div>
